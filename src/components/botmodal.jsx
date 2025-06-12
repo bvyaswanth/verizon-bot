c04fd848ad39c31e6a484477afc3d0014b4507b2
bvyaswanth <bvyaswanth@gmail.com>
2025-06-12 13:14:48 -0500

    Added modal routing

--- src/components/botmodal.jsx
+++ src/components/botmodal.jsx
@@ -2,12 +2,9 @@ const BotModal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div
-            onClick={onClose}
-            className="fixed inset-0 w-full h-full bg-[#00000080] bg-opacity-50 flex items-center justify-center"
-        >
-            <div
-                className="bg-white h-[580px] w-[1000px] mx-auto p-[2%] border-2 border-black rounded-[10px] shadow-md">
+        <div className="fixed inset-0 w-full h-full bg-[#00000080] bg-opacity-50 flex items-center justify-center">
+            <div className="bg-white h-[580px] w-[1000px] mx-auto p-[2%] border-2 border-black rounded-[10px] shadow-md">
+                <button onClick={onClose}>X</button>
                 {children}
             </div>
         </div>
